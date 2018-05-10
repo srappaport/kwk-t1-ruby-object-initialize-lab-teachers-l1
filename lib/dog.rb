93550320637443473d2b437af7fd30cf9ab137e2
srappaport <sarah.rappaport@greendot.org>
2018-05-10 23:15:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog
   def initialize(dog_name)
-    @dog = dog_name
+    @name = dog_name
   end
 
-  def breed=(breed)
-    @breed = breed
+  def name=(name)
+    @name = name
   end
 
-  def breed
-    @breed
+  def name
+    @name
   end
 end
